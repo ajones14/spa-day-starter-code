d8e258c053e5bc7f50fcdbf39f203d48a8993ec0
Abigail Wiggins <rebtaylor14@gmail.com>
2019-12-19 03:25:07 -0600

    Studio p1&2 completed

--- src/main/java/org/launchcode/spaday/controllers/UserController.java
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -1,12 +1,27 @@
 package org.launchcode.spaday.controllers;
 
+import org.launchcode.spaday.models.User;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("user")
 public class UserController {
 
+    @GetMapping("add")
+    public String displayAddUserForm() {
+        return "user/add";
+    }
 
+    @PostMapping
+    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
+        model.addAttribute("name", user.getUsername());
+        if (user.getPassword().equals(verify)) {
+            return "user/index";
+        } else {
+            return "user/add";
+        }
+    }
 
 }
